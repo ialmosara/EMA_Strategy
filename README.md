8874ab7b65312de84be96818aa933ee47705020f
manny <ialmosara@gmail.com>
2021-05-08 10:58:27 -0400

    Established connection to DB and alpaca - need to push data to db and stream data

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 This app is developed by Immanuel Almosara (ialmosara) and Amitpal Gill (Amit97-ops). The idea behind this app is to utilize the 9, 21, and 55 EMA (Exponential Moving Average) to generate buying and selling signals.
 
 ## Technolgies used
-The technologies used in this project include: Python, Alpaca API
+The technologies used in this project include: 
+* Python Flask
+* Alpaca API (https://github.com/alpacahq/alpaca-trade-api-python/tree/9c78d4122c7d396f014435feed4058953f6612c0)
+* PostgreSQL
 
 ## Signal Rules
 If the 9 EMA crosses above the 21 and 55 EMA then it will trigger a buy signal.
--- api.py
+++ api.py
@@ -1,14 +1,48 @@
 import alpaca_trade_api as tradeapi
-import talib
-import numpy
+from alpaca_trade_api.rest import TimeFrame
 from decouple import config
+from datetime import datetime, timezone, timedelta
 from flask import Flask, request
+import psycopg2
 
 app = Flask(__name__)
 
-@app.run('/run=<symbol>')
-def run(self):
-    pass
+ENDPOINT = config('ENDPOINT')
+PAPER_ENDPOINT = config('PAPER_ENDPOINT')
+SECRET_KEY = config('SECRET_KEY')
+API_KEY = config('API_KEY_ID')
+PAPER_STREAM_ENDPOINT = config('PAPER_STREAM_ENDPOINT')
+
+alpaca = tradeapi.REST(API_KEY, SECRET_KEY, PAPER_ENDPOINT, 'v2')
+stream = tradeapi.stream.TradingStream(API_KEY, SECRET_KEY, PAPER_STREAM_ENDPOINT)
+
+conn = psycopg2.connect(host='localhost',
+                        database='emastrategy',
+                        user='manny',
+                        password='emastrategy')
+
+@app.route('/subscribe')
+def subscribe():
+    # Used for subscribing to data
+    return
+
+@app.route('/get_data')
+def get_data():
+    # Fetching historical data
+    j = request.get_json()
+    local_time = datetime.now()
+    # Parse timestamps (Last 3 months to compensate for 55 EMA)
+    end_time, start_time = local_time.isoformat('T')+"Z", (local_time-timedelta(days=90)).isoformat('T') + "Z"
+    bars = alpaca.get_bars(symbol=j['symbol'], timeframe=TimeFrame.Day, start=start_time, end=end_time)
+    for bar in bars:
+        print(bar)
+    return ''
+
+@app.route('/run')
+def run():
+    r = request.json()
+    alpaca.cancel_all_orders()
+    alpaca.submit_order(symbol=r['symbol'], qty=r['qty'], side=r['side'], type=r['type'], time_in_force=r['time_in_force'])
     
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1', port='90')
+    app.run(port=90)
